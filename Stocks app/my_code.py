d29fb4112911f276ea5d3897c7e7bdfe8bd6d351
ahvenainen <121426005+ahvenainen@users.noreply.github.com>
2023-04-19 21:26:39 +0300

    changes to notes

--- Stocks app/my_code.py
+++ Stocks app/my_code.py
@@ -1,59 +1,9 @@
 """
-KT2
-
-
 Olet aloittanut osakesijoittamisen ja haluat arvioida sijoituksesi arvoa.
 Ohjelmalla (pääohjelmassa) on lista, johon käyttäjä voi lisätä Osake-olioita.
 Ohjelma kysyy käyttäjältä ”Lisätäänkö uusi osake (k/e)”.
 Kun osakkeet on lisätty listaan, kysyy ohjelma käyttäjältä kuvitteellisen kasvuprosentin sekä ajanjakson vuosina.
-
-
-Tee luokka Osake, jolla on jäsenmuuttujat:
-- nimi
-- ostohinta (>0, osakekohtainen ostohinta)
-- maara (> 0, omistettujen osakkeiden lkm)
-
-
-
-Osakkeella on metodit:
-
-- tulosta_arvo, jonka parametreina on kasvuprosentti ja ajanjakso vuosina (tässä järjestyksessä).
-tulosta_arvo-metodi kutsuu toista Osake-luokan metodia laske_tuotto_yhdelle_vuodelle, joka laskee vuosikohtaisen tuoton
-tulosta_arvo kutsuu laske_tuotto_yhdelle_vuodelle-metodia vuosien lukumäärän verran.
-Siis, jos lasketaan tuottoa kolmelle vuodelle, niin laske_tuotto_yhdelle_vuodelle kutsutaan kolme kertaa.
-Huomioi "korkoa korolle". Valuutat tulostetaan kahden desimaalin tarkkuudella.
-
-- laske_tuotto_yhdelle_vuodelle -metodi palauttaa tuoton yhdelle vuodelle.
-Metodi on staattinen ja saa parametrinaan kasvuprosentin ja hinnan vuoden alussa (tässä järjesyksessä).
-
-Laske pääohjelmassa  myös koko osakepotin arvo(sama % ja samat vuodet) käymällä lista läpi eli 
-joudut miettimään sitä, miten pääohjelmaan palautetaan tieto yhden osakkeen potin arvosta vuosien jälkeen.
-
-Esimerkkiajo:
-
-
-Anna osakkeen nimi: Nokia
-Anna osakkeen ostohinta/kpl: 10
-Anna ostettujen osakkeiden lukumäärä: 1000
-Lisää osakkeita (k/e)? k
-
-Anna osakkeen nimi: Fortum
-Anna osakkeen ostohinta/kpl: 12
-Anna ostettujen osakkeiden lukumäärä: 127
-Lisää osakkeita (k/e)? e
-
-Anna kasvuprosentti: 5
-Anna vuodet: 3
-
-Nokia 1000 10.0
-Osakkeen potin arvo on 11576.25 ja tuotto 1576.25
-
-Fortum 127 12.0
-Osakkeen potin arvo on 1764.22 ja tuotto 240.22
-
-Koko potin arvo on 13340.47
 """
-# Write class and imports here!
 
 
 class Osake:
